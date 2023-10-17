597fe5e1011c969170837577b5a57b4e237d9f43
Spandan Mukherjee <95229740+SpandanM110@users.noreply.github.com>
2023-10-17 19:19:57 +0530

    Update About.js

--- incity/city/src/Components/About.js
+++ incity/city/src/Components/About.js
@@ -4,14 +4,14 @@ import AboutBackgroundImage from "../Assets/about-background-image.png";
 import { BsFillPlayCircleFill } from "react-icons/bs";
 import Testimonial from "./Testimonial";
 
-
 const About = () => {
-  return (
+  // Define the URL of your video
+  const videoUrl = "https://youtu.be/lSkOCPNfNiI?feature=shared";
 
+  return (
     <div className="about-section-container">
       <div className="about-background-image-container">
         <img src={AboutBackground} alt="" />
-        
       </div>
       <div className="about-section-image-container">
         <img src={AboutBackgroundImage} alt="" />
@@ -22,38 +22,36 @@ const About = () => {
           Managing and Working in a city is easy to do with INCITYPRO
         </h1>
         <p className="primary-text">
-          The main abmition is to make a platform for all the city needs and the user need not to install much application and the future is to make it most flexible and relaible source people can access even entertainment space will be there for the users to enjoy.
+          The main ambition is to make a platform for all the city needs and the user need not to install much application, and the future is to make it the most flexible and reliable source people can access even entertainment space will be there for the users to enjoy.
         </p>
-        <button
-          className="inline-button"
-          style={{
-            padding: "0.8rem 2rem",
-            backgroundColor: "blue",
-            outline: "none",
-            border: "none",
-            borderRadius: "5rem",
-            fontSize: "1.1rem",
-            cursor: "pointer",
-            fontWeight: "600",
-            color: "white",
-            transition: "0.2s",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          Watch Video <BsFillPlayCircleFill style={{ marginLeft: "0.5rem" }} />
-        </button>
-        
-      
+        <a href={videoUrl} target="_blank" rel="noopener noreferrer">
+          <button
+            className="inline-button"
+            style={{
+              padding: "0.8rem 2rem",
+              backgroundColor: "blue",
+              outline: "none",
+              border: "none",
+              borderRadius: "5rem",
+              fontSize: "1.1rem",
+              cursor: "pointer",
+              fontWeight: "600",
+              color: "white",
+              transition: "0.2s",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Watch Video <BsFillPlayCircleFill style={{ marginLeft: "0.5rem" }} />
+          </button>
+        </a>
+
         <br />
         <br />
         <br />
-      
       </div>
-        
     </div>
-    
   );
 };
 
